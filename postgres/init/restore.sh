d23e10293b49d0f3dc0ca8f12e81a6cc83079087
julien-data-analyst-V2 <julienrenoult76460@gmail.com>
2026-06-03 16:56:15 +0200

    fix(docker-compose): modification du docker compose prod pour que ça s'exécute côté windows.

--- postgres/init/restore.sh
+++ postgres/init/restore.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
 set -e
 
 echo "Restauration du dump Metabase..."
 
-pg_restore \
-  --username="$POSTGRES_USER" \
-  --dbname="metabase" \
-  /docker-entrypoint-initdb.d/metabase.dump
+pg_restore --username="$POSTGRES_USER" --dbname="metabase" /docker-entrypoint-initdb.d/metabase.dump
 
 echo "Restauration terminée."
--- postgres/restore.sh
+++ postgres/restore.sh
@@ -3,9 +3,6 @@ set -e
 
 echo "Restauration du dump Metabase..."
 
-pg_restore \
-  --username="$POSTGRES_USER" \
-  --dbname="metabase" \
-  /docker-entrypoint-initdb.d/metabase.dump
+pg_restore --username="$POSTGRES_USER" --dbname="metabase" /docker-entrypoint-initdb.d/metabase.dump
 
 echo "Restauration terminée."
